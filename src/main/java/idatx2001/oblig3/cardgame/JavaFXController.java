c00498151c0fc3dc6565f293442eeff6e566c933
Leo <leonardschiller420@gmail.com>
2021-04-11 17:45:16 +0200

    Updated controller for functionality

--- src/main/java/idatx2001/oblig3/cardgame/JavaFXController.java
+++ src/main/java/idatx2001/oblig3/cardgame/JavaFXController.java
@@ -3,6 +3,7 @@ package idatx2001.oblig3.cardgame;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import java.io.FileNotFoundException;
 
@@ -43,4 +44,25 @@ public class JavaFXController {
 
     @FXML
     private Button checkHand;
+
+    public void dealHandButtonOnClick(){
+        dealHand();
+        displayCards();
+        sumOfFaces.setText(Integer.toString(hand.sumCheck()));
+        cardsOfHearts.setText(hand.heartsCheck());
+        flush.setText(hand.flushCheck());
+        queenOfSpades.setText(hand.queenOfSpadesCheck());
+    }
+
+    public void dealHand(){
+        this.hand = new Hand(deck.dealHand(5));
+    }
+
+    public void displayCards(){
+        card0.setImage(new Image(this.getClass().getResourceAsStream("/cards/PNG/" + hand.getCardAt(0).getAsString() + ".png")));
+        card1.setImage(new Image(this.getClass().getResourceAsStream("/cards/PNG/" + hand.getCardAt(1).getAsString() + ".png")));
+        card2.setImage(new Image(this.getClass().getResourceAsStream("/cards/PNG/" + hand.getCardAt(2).getAsString() + ".png")));
+        card3.setImage(new Image(this.getClass().getResourceAsStream("/cards/PNG/" + hand.getCardAt(3).getAsString() + ".png")));
+        card4.setImage(new Image(this.getClass().getResourceAsStream("/cards/PNG/" + hand.getCardAt(4).getAsString() + ".png")));
+    }
 }

fce54eb2d05f5a9a3c6f92b9a14e13ebd7d2654d
Leo <leonardschiller420@gmail.com>
2021-04-11 17:31:20 +0200

    Oppgave 5

--- src/main/java/idatx2001/oblig3/cardgame/Hand.java
+++ src/main/java/idatx2001/oblig3/cardgame/Hand.java
@@ -17,5 +17,33 @@ public class Hand {
     public PlayingCard getCardAt(int index) {
         return hand.get(index);
     }
+
+    public int sumCheck(){
+        return hand.stream().map(PlayingCard::getFace).reduce(Integer::sum).get();
+    }
+
+    public String heartsCheck(){
+        StringBuilder heartCheck = new StringBuilder();
+        hand.stream().filter(p -> p.getSuit() == 'H').forEach(p -> heartCheck.append(p.getAsString() + ", "));
+        if(heartCheck.equals("")){
+            return "No Hearts";
+        }
+        return heartCheck.toString();
+    }
+
+    public String queenOfSpadesCheck(){
+        if (hand.stream().anyMatch(playingCard -> playingCard.getAsString().equalsIgnoreCase("S12"))) {
+            return "Yes";
+        }else{
+            return "No";
+        }
+    }
+    public String flushCheck(){
+        if (hand.stream().allMatch(a -> a.getSuit() == hand.get(0).getSuit())){
+            return "Yes";
+        }else{
+            return "No";
+        }
+    }
 }
 
